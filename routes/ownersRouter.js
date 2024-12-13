aa9a8136250e5cd286cc23e825eb9213310a5440
Aawhan Vyas <vyasaawhan@gmail.com>
2024-12-13 12:09:38 +0530

    13 dec first commit

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -2,17 +2,28 @@ const express = require("express");
 const router = express.Router();
 const ownerModel = require("../models/owner-model")
 
-router.get("/", (req,res)=> {
-    res.send("hii, working fine");
-});
-
 if(process.env.NODE_ENV==="development"){
-    router.post("/create", (req,res)=>{
-        res.send("Hey its workingg!!")
+    router.post("/create", async (req,res)=>{
+        
+        let owners= await ownerModel.find();
+        if(owners.length>0){
+            return res
+            .status(504)
+            .send("You dont have permission to createa a new owner")
+        }
+
+        let {fullname, email, password} = req.body; 
+        let createdOwner = await ownerModel.create({
+            fullname: String,
+            email: String,
+            password: String,    
+        })
+        res.status(201).send(createdOwner);
     });
 }
 
-
-    
+router.get("/", (req,res)=> {
+    res.send("hii, working fine. This is owners route");
+});
 
 module.exports = router;
